af5ed4578897f381142b8a55d2981a9e0322b73d
HMaroba <marobahlalele910@gmail.com>
2024-07-31 11:43:36 +0200

    Modified app

--- employee_register/models.py
+++ employee_register/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Position(models.Model):
+    title = models.CharField(max_length=50)
+class Employee(models.Model):
+    fullName = models.CharField(max_length=100)
+    emp_code = models.CharField(max_length=100)
+    mobile = models.CharField(max_length=100)
+    position = models.CharField(max_length=100)
