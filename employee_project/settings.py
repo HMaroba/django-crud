24143f59025866ed52e87d27214119579d5a3d6f
HMaroba <marobahlalele910@gmail.com>
2024-07-31 11:28:56 +0200

    Initial commit

--- employee_project/settings.py
+++ employee_project/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'employee_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'djangoEmployeeDB',
+        'USER' : 'postgres',
+        'PASSWORD' : 'Maroba@2024',
+        'HOST' : 'localhost',
     }
 }
 
