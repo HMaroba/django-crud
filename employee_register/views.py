24b40c92aebda90125132697bdbd134ecf2d51a5
HMaroba <marobahlalele910@gmail.com>
2024-07-31 16:14:26 +0200

    Created notes api app

--- employee_register/views.py
+++ employee_register/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from employee_register.forms import EmployeeForm
 
 # Create your views here.
 
@@ -8,7 +7,6 @@ def employee_list(request):
     return render(request, "employee_register/employee_list.html")
 
 def employee_form(request):
-    form = EmployeeForm
     return render(request, "employee_register/employee_form.html")
 
 def employee_delete(request):
