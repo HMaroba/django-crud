ed8950a7f3ec9bc46d6a25c7345052faccea3732
HMaroba <marobahlalele910@gmail.com>
2024-07-31 11:38:16 +0200

    Modified app

--- employee_project/settings.py
+++ employee_project/settings.py
@@ -76,12 +76,16 @@ WSGI_APPLICATION = 'employee_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'djangoEmployeeDB',
-        'USER' : 'postgres',
-        'PASSWORD' : 'Maroba@2024',
-        'HOST' : 'localhost',
-    }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    },
+    #       'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': 'djangoEmployeeDB',
+    #     'USER' : 'postgres',
+    #     'PASSWORD' : 'Maroba@2024',
+    #     'HOST' : 'localhost',
+    # }
 }
 
 
