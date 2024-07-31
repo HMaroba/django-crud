e78c864f586d83a150ff48d692213ef701b1447d
HMaroba <marobahlalele910@gmail.com>
2024-07-31 16:10:20 +0200

    Created notes api app

--- employee_project/urls.py
+++ employee_project/urls.py
@@ -20,4 +20,5 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('employee/',include('employee_register.urls')),
+     path('api/v1/notes/', include('note_api.urls'))
 ]
